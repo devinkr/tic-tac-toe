0d6b0f0753c116ce60be52141a0c22b7301f92b4
Devin Raleigh <devinkr@gmail.com>
2022-05-24 16:48:56 -0600

    Complete checkForWinner

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 /*----- constants -----*/
 const PLAYER1_MARK = 'red';
 const PLAYER2_MARK = 'blue';
+const WINNING_ARRAY = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[1, 5, 9],
+	[3, 5, 7],
+	[1, 4, 7],
+	[2, 5, 8],
+	[3, 6, 9],
+];
 
 /*----- app's state (variables) -----*/
 let currentPlayer;
 const player1Boxes = [];
 const player2Boxes = [];
+const player1Score = 0;
+const player2Sore = 0;
 
 /*----- cached element references -----*/
 const mainEl = document.querySelector('main');
@@ -42,14 +54,36 @@ function changeGrid(element) {
 			alert('Please pick a different box.');
 		} else if (currentPlayer === 1) {
 			player1Boxes.push(boxId);
+			checkForWinner();
 			box.style.backgroundColor = PLAYER1_MARK;
 			currentPlayer = 2;
 			playerEl.innerText = currentPlayer;
 		} else {
 			player2Boxes.push(boxId);
+			checkForWinner();
 			box.style.backgroundColor = PLAYER2_MARK;
 			currentPlayer = 1;
 			playerEl.innerText = currentPlayer;
 		}
 	}
+}
+
+function checkForWinner() {
+	let playerBoxes;
+	if (currentPlayer === 1) {
+		playerBoxes = player1Boxes;
+	} else {
+		playerBoxes = player2Boxes;
+	}
+	// Check each winning combo in WINNING_ARRAY and see if combo exists in player's saved boxes.
+	// Found at https://bobbyhadz.com/blog/javascript-check-if-array-contains-all-elements-another-array
+	for (let i = 0; i < WINNING_ARRAY.length; i++) {
+		const containsAll = WINNING_ARRAY[i].every((element) => {
+			return playerBoxes.includes(element);
+		});
+		if (containsAll === true) {
+			return true;
+		}
+	}
+	return false;
 }
